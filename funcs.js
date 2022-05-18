2b74b139b857169aca38370091c2ddc03b7bb2aa
Stifmaister03 <105817908+Stifmaister03@users.noreply.github.com>
2022-05-18 14:03:41 -0500

    Add files via upload

---
+++ funcs.js
@@ -0,0 +1,16 @@
+function cTodo1() {
+     if ((document.getElementById("titulo").innerHTML) = "La divina comedia");  //**Revise la parte innerALGO, esta mal.
+     document.getElementById("pix").src = "https://th.bing.com/th/id/R.d4385b5daf057b81288d0fee4187d131?rik=k5FYXmlIpf3h7g&riu=http%3a%2f%2fresumiendolo.com%2fwp-content%2fuploads%2f2018%2f02%2fla-divina-comedia12-1024x768.jpg&ehk=S86F6JKGlHYJyL%2f1S42Yg3zlc8dCVYsc4vh0jARXuug%3d&risl=&pid=ImgRaw&r=0";                //**Revise la parte "fotito", esta mal llamado el elemento imagen, cual es su ID?
+     document.getElementById("infr").innerHTML = "Por Dante Alighieri, el gran poeta italiano que nos trae esta obra que hoy es mi libro favorito; La divina comedia";  //**Revise la sintaxis de DOCUMENT..
+}
+function cTodo2() {
+    if ((document.getElementById("titulo").innerHTML) = "Oliver Ruiz");
+     document.getElementById("pix").src = "https://scontent.fbjx1-1.fna.fbcdn.net/v/t39.30808-6/243127131_1858763694300540_5296412607375695081_n.jpg?_nc_cat=100&ccb=1-6&_nc_sid=09cbfe&_nc_eui2=AeENhvE9tr7WoJY6cTHCP7Bl17_lDvoIhHjXv-UO-giEeGw3VA8yVnat-BcW1W_zJLikCtst9mCxk3VpSoF10q7d&_nc_ohc=8FtPoulpGZQAX-cm2f-&_nc_ht=scontent.fbjx1-1.fna&oh=00_AT_8OXR-ocZ3EpJe8Xtk6VsV9KXXTcxnvTTcruhil0LYRA&oe=628A1C4B";
+     document.getElementById("infr").innerHTML = "Soy un estudiante de preparatoria, me gusta la literatura, la programación, las motocicletas, los tatuajes y el ajedrez";
+}
+function OcultaR() {
+     document.getElementById('logito').style.display='none';    //**Revise la parte 'NINGUNO?' , esta mal.
+}
+function MuestraR() {
+    document.getElementById('logito').style.display='block';     //**Revise la parte 'BLOQUEO!' , esta mal.
+}
